7c0d0bfef2de6102466a5971e7825bdb3c670077
matt-cassinelli <67283034+matt-cassinelli@users.noreply.github.com>
2022-05-28 21:33:40 +0100

    Improve card loading

--- Client/script.js
+++ Client/script.js
@@ -1,31 +1,17 @@
 const host = "https://localhost:5001";
+// const soundsNode = document.getElementById("card_container");
+const soundsNode = document.querySelector('.card_container');
+const container = document.querySelector('.container');
+var sounds = Array();
 
-document.addEventListener("DOMContentLoaded", () => {
-    renderSounds(true);
+function renderSounds() {
     fetch(`${host}/sounds`, {
         method: "GET"
     })
-    .then(resp => resp.json()) // TODO: Can we reduce the lines here?
-    .then(data => sounds = data)
-    .then(() => renderSounds(false))
-    .catch(err => console.log(err));
-});
-
-// const soundsNode = document.getElementById("card_container");
-const soundsNode = document.querySelector('.card_container');
-
-var sounds = Array();
-
-function renderSounds(loading) {
-    if (loading) {
-        soundsNode.innerHTML = 
-            `<div id="loading">
-                <img src="media/img-app/loading-dark.png" alt="loading" />
-            </div>`
-        ;
-    } else {
-        soundsNode.innerHTML = null; // Clear loading animation
-        sounds.forEach(sound => {
+    .then(response => response.json())
+    // .then(resp => console.log(resp.json()))
+    .then(data => 
+        data.forEach(sound => {
             soundsNode.innerHTML += // TODO: Add Price instead of duration. Put to the left of/inside button.
                 `<div class="card"> 
                     <h3 class="card__header">${sound.title}</h3> <!-- TODO: use inheritance / targetting instead of classes -->
@@ -34,6 +20,15 @@ function renderSounds(loading) {
                     <button class="card__button" onclick="customise(${sound.id})">Customise</button>
                 </div>`
             ;
-        });
-    };
+        })
+    )
+    .catch(err => console.log(err))
+    .finally( () => document.querySelector('#loading').style.display = "none" )
 };
+
+function renderTags() {};
+
+document.addEventListener("DOMContentLoaded", () => {
+    renderSounds();
+    renderTags();
+});
