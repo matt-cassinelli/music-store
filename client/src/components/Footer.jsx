1881392a52276cf2ca8e439c5ce202fdf172e597
matt-cassinelli <mdcassinelli@outlook.com>
2022-12-19 01:22:57 +0000

    Add simple Footer

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import './Footer.css';
+
+export default function Footer() {
+    return (
+        <div id='footer'>
+            <h5> &copy; {new Date().getFullYear()} Matt Cassinelli</h5>
+        </div>
+    )
+}
