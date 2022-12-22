4d544e69c534b4dc00f9e530347cb4b6be52d291
matt-cassinelli <mdcassinelli@outlook.com>
2022-12-22 15:43:55 +0000

    Change some svg files to inline code

--- client/src/components/Navbar/BasketButton.jsx
+++ client/src/components/Navbar/BasketButton.jsx
@@ -1,4 +1,3 @@
-import { ReactComponent as BasketIcon } from './basket.svg';
 import { Link } from 'react-router-dom';
 import './BasketButton.css';
 
@@ -9,7 +8,10 @@ export default function BasketButton({className}) {
     return (
         <Link to="/basket" className={'basket-btn-container ' + (className || "")}>
             {/* <img src={basket} alt="basket" /> */}
-            <BasketIcon className='basket-svg'/>
+            <svg id="layer-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 902.86 748.42" className='basket-svg'>
+                <path d="M231.36,577.83H828.11L902.86,178.7l-703.13.5-26-102H0v68H120.87L231.36,577.83ZM820.93,246.76,771.66,509.83H284.17L217.09,247.19Z" transform="translate(0 -77.22)"/>
+                <path d="M433.19,716.9a108.32,108.32,0,0,0-7.93-40.75H633.33a108.74,108.74,0,1,0,100.81-68H324.44A108.75,108.75,0,1,0,433.19,716.9Zm301-40.75A40.75,40.75,0,1,1,693.4,716.9,40.8,40.8,0,0,1,734.14,676.15Zm-409.7,0A40.75,40.75,0,1,1,283.7,716.9,40.79,40.79,0,0,1,324.44,676.15Z" transform="translate(0 -77.22)"/>
+            </svg>
             <span>7</span>
         </Link>
     )
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 //import CartButton from './CartButton';
 import logo from '../logo.svg';
 // import menu from '../assets/img-app/menu.svg';
-import { ReactComponent as MenuIcon } from './menu.svg';
+import MenuButton from './MenuButton';
 import ThemeButton from './ThemeButton';
 import BasketButton from './BasketButton';
 
@@ -26,10 +26,7 @@ export default function Navbar() {
             <div className='header-side'>
                 <ThemeButton />
                 <BasketButton className="hide-on-min-width"/>
-                <button type='button' className='menu-button'> {/* Can I use a <button> for the theme btn too? */}
-                    {/* <img src={menu} alt="Menu"/> */}
-                    <MenuIcon className='menu-svg' alt="Menu"/>
-                </button>
+                <MenuButton />
             </div>
 
         </header>
